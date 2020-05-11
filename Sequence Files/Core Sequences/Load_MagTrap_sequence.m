b39a5dd2e10642078b635db98a836772caa81af5
PeihangXu <Richardxu93@gmail.com>
2020-05-10 23:45:04 -0400

    Work until May 10 2020

--- Sequence Files/Core Sequences/Load_MagTrap_sequence.m
+++ Sequence Files/Core Sequences/Load_MagTrap_sequence.m
@@ -239,7 +239,7 @@ initialize_channels();
     seqdata.flags.compensation_in_modulation = 0; %turn on a compensation beam
 
     % Optical lattice
-    seqdata.flags.load_lattice = 0; % set to 2 to ramp to deep lattice at the end; 3, variable lattice off & XDT off time
+    seqdata.flags.load_lattice = 1; % set to 2 to ramp to deep lattice at the end; 3, variable lattice off & XDT off time
     seqdata.flags.pulse_lattice_for_alignment = 0; % 1: lattice diffraction, 2: hot cloud alignment, 3: dipole force curve
     seqdata.flags.pulse_zlattice_for_alignment = 0; % 1: pulse z lattice after ramping up X&Y lattice beams (need to plug in a different BNC cable to z lattice ALPS)
 

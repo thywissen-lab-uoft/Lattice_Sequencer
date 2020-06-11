03bfad21e3cf065b33b3e11c9ef320e8b1546a89
RhysAnderson <rhys.anderson@mail.utoronto.ca>
2020-06-11 14:13:36 -0400

    Made a few more changes to absorption_image. Holding off on changes to Load_MagTrap_sequence for now.

--- Sequence Files/Load Magtrap Sequence Files/Load_Absorption_Image_Flags.m
+++ Sequence Files/Load Magtrap Sequence Files/Load_Absorption_Image_Flags.m
@@ -1,39 +1,23 @@
-%Global comments:
-%Need to remove some of these in Load_MagTrap_sequence
-%Do we need image_type?
-%Make this as compact as possible
+function flags = Load_Absorption_Image_Flags()
+    
+    flags.image_atomtype = 'K';%seqdata.flags.image_atomtype; %Atom species being imaged: 'K' or 'Rb'
+    flags.img_direction = 'X';%seqdata.flags.img_direction; %Which lattice direction the atoms are imaged in: 1 = x-cam, 2 = y-cam
+    flags.condition = 'SG'; %'normal', 'in_trap', 'QP_imaging', 'SG', 'short_tof'
+    flags.negative_imaging_shim = 'positive'; %0 = positive states 1 = negative states %Automatically set by K_RF_Sweep flag
 
-image_loc = 1; %Location of the image: 0 = MOT, 1 = science chamber
-image_atomtype = 1; %Atom species being imaged: 0 = Rb, 1 = K, 2 = Both
-%Delete the others? %1 = x direction (Sci) / MOT, 2 = y direction (Sci), %3 = vertical direction, 4 = x direc tion (has been altered ... use 1), 5 = fluorescence(not useful for iXon)
-img_direction = 1; %Which lattice direction the atoms are imaged in: 1 = x-cam, 2 = y-cam
-do_stern_gerlach = 0; %Whether to pulse the QP coil to split spins during ToF (takes some time,setting minimum ToF): 0 = No SG, 1 = SG
-iXon = 0; %Use iXon camera to take an absorption image (only vertical)
-do_F1_pulse = 1; %Repump Rb F = 1 to F = 2 before/during imaging
-In_Trap_imaging = 0; %Set to take the image while the atoms are still in the magnetic trap
-High_Field_Imaging = 0; %Set to image the atoms at a field near the FB resonance (near 202.1G)
-perp_quant_field = 0; %set to nonzero integer (1) to use a quantization field perpendicular to img_direction
-
-use_K_OP = 1; %Usually useful. Must enable repump as well.
-use_K_repump = 1; % 1:turn on K repump beam for imaging F=7/2
-use_K_repump_at_low_field = 0; %Ever used?
-K_repump_during_image = 0; %Not sure this is useful.
-negative_imaging_shim = 0; %0 = positive states 1 = negative states %Automatically set by K_RF_Sweep flag
-
-
-if seqdata.flags.High_Field_Imaging==0 %disable the optical pumping during HF imaging
-    use_K_OP = 0;
+    flags.image_loc = 1;
+    flags.iXon = 0; %Use iXon camera to take an absorption image (only vertical)
+    flags.do_F1_pulse = 1; %Repump Rb F = 1 to F = 2 before/during imaging
+    flags.High_Field_Imaging = 0; %Set to image the atoms at a field near the FB resonance (near 202.1G)
+    flags.rb_vert_insitu_image = 0; %Take an absorption image of the BEC up through the system to centre objective.
+    
+    %These flags were already isolated in absorption_image.
+    flags.use_K_OP = 1; %Usually useful. Must enable repump as well.
+    flags.use_K_repump = 1; % 1:turn on K repump beam for imaging F=7/2
+    flags.K_repump_during_image = 0; %Not sure this is useful.
+    flags.perp_quant_field = 0; %set to nonzero integer (1) to use a quantization field perpendicular to img_direction    quant_handover_time = 3; % time to ramp shims for imaging ... this may be changed below depending on the selected option    
+    
+        seqdata.flags.rb_vert_insitu_image = 0; 
+    %take a vertical in-situ image of BEC in XDT to centre the microscope objective
+    
 end
-
-%Take an image after short time of flight
-short_tof = 0;
-%Take an in-situ image of the cloud after D1 cooling (need to leave a wait
-%time after the molasses >3ms)
-in_situ_D1 = 0;
-%         if seqdata.flags. image_type == 8
-%             in_situ_D1 = 1;
-%         end
-%Image with 405nm Beam
-blue_image = 0;
-%Image with D1 beam
-D1_image = 0;
--- Sequence Files/Load Magtrap Sequence Files/Load_Absorption_Image_Parameters.m
+++ Sequence Files/Load Magtrap Sequence Files/Load_Absorption_Image_Parameters.m
@@ -1,25 +1,46 @@
-detuning.K.X.positive.normal = 42;
-detuning.K.X.positive.in_trap = 44;
-detuning.K.X.positive.QP_imaging = 42;
-detuning.K.X.positive.SG = 45;
-detuning.K.X.positive.short_tof = 45;
-detuning.K.X.negative.normal = 51;
-detuning.K.X.negative.SG = 50;
+function params = Load_Absorption_Image_Parameters()
 
-detuning.K.Y.positive.normal = 42;
-detuning.K.Y.negative.normal = 52;
-
-detuning.K.MOT.positive.normal = 41;
-detuning.K.MOT.positive.short_tof = 54.5;
-
-detuning.Rb.X.positive.normal = 6590 - 238;
-detuning.Rb.X.positive.in_trap = 6590 - 246;
-detuning.Rb.X.positive.QP_imaging = 6590 - 238.5;
-detuning.Rb.X.positive.SG = 6590 - 241.8;
-
-detuning.Rb.X.negative = 6590 - 232;
-
-detuning.Rb.Y.positive.normal = 6590 - 230.7;
-detuning.Rb.Y.positive.in_trap = 6590 - 243;
-
-detuning.Rb.MOT.positive.normal = 6590 - 240;
+    %% Set imaging detunings
+    % Potassium - X-cam
+    params.detunings.K.X.positive.normal = 42;
+    params.detunings.K.X.positive.in_trap = 44;
+    params.detunings.K.X.positive.QP_imaging = 42;
+    params.detunings.K.X.positive.SG = 45;
+    params.detunings.K.X.positive.short_tof = 45;
+    params.detunings.K.X.negative.normal = 51;
+    params.detunings.K.X.negative.SG = 50;
+    % Potassium - Y-cam
+    params.detunings.K.Y.positive.normal = 42;
+    params.detunings.K.Y.negative.normal = 52;
+    % Potassium - MOT
+    params.detunings.K.MOT.positive.normal = 41;
+    params.detunings.K.MOT.positive.short_tof = 54.5;
+    % Rubidium - X-cam
+    params.detunings.Rb.X.positive.normal = 6590 - 238;
+    params.detunings.Rb.X.positive.in_trap = 6590 - 246;
+    params.detunings.Rb.X.positive.QP_imaging = 6590 - 238.5;
+    params.detunings.Rb.X.positive.SG = 6590 - 241.8;
+    params.detunings.Rb.X.negative = 6590 - 232;
+    % Rubidium - Y-cam
+    params.detunings.Rb.Y.positive.normal = 6590 - 230.7;
+    params.detunings.Rb.Y.positive.in_trap = 6590 - 243;
+    % Rubidium - MOT
+    params.detunings.Rb.MOT.positive.normal = 6590 - 240;
+    
+    %% Probe beam powers
+    params.powers.K.X = 0.09;
+    params.powers.K.Y = 0.05;
+    params.powers.K.MOT = 0.8;
+    params.powers.Rb.X = 0.3;
+    params.powers.Rb.Y = 0.25;
+    params.powers.Rb.MOT = 0.25;
+    
+    %% Timing parameters
+    params.pulse_length = 0.3;
+    params.K_OP_time = 0.3;
+    params.k_detuning_shift_time = 0.5;
+    params.rb_detuning_shift_time = 4;
+    
+    %% Other parameters
+    params.RB_FF = 1.2;
+end

2220945fb7e47c6a47ab0d4acde42eb2b73c1c69
Cora <63414958+cjfujiwara@users.noreply.github.com>
2020-06-26 20:21:15 -0400

    comments
    adding additional comments to thre previous implementation of the code.

--- GUI Functions/plotgui.m
+++ GUI Functions/plotgui.m
@@ -1,15 +1,28 @@
 function hFGUI=plotgui
-%PLOTGUI Summary of this function goes here
+% plotgui.m
+%
+% Author CJ : Fujiwara
+% Version   : 0.2
+%
+% This code open a GUI for the Lattice Sequencer computer.  It primary
+% intent is to enable a visual interface for the user to view relevant
+% sequence parameters in a graphical way.
 
-%   Detailed explanation goes here
-% Version control string
-version='0.1';
+version='0.2';
 str=['PlotGUI_v' version];
+
+%% Fetch information from the primary GUI
 
 % Intiailize sequence data
 global seqdata;
 start_new_sequence();
 initialize_channels();
+% In the future, the plotgui should probably have seqdata as an input
+% argument. If we save the seqdata with every single run of the machine,
+% then we would probably want to visually compares different sequences of
+% the machine.
+
+
 
 %%%%%%%%%%%%%%%%%%% TERRIBLE
 % fetch seuqnece function
@@ -47,7 +60,7 @@ aCHshow(:)=[];
 dCHshow=seqdata.digchannels(1);
 dCHshow(:)=[];
 
-%%%%%%%%%%%%%%% Graphical Objects %%%%%%%%%%%%
+%%%%%%%%%%%%%%% Primary graphical objects %%%%%%%%%%%%
 
 % Initialize main figure
 hFGUI=figure(99);
@@ -57,24 +70,25 @@ set(hFGUI,'color','w','Name',str,'Toolbar','none','menubar','none',...
 hFGUI.Position(3:4)=[500 600];
 hFGUI.Position(2)=50;
 
+% Initialize uipanel that contains all channel information
 hpAllChannels=uipanel('parent',hFGUI,'backgroundcolor','w',...
     'units','pixels','Title','All Channels','fontsize',12);
 hpAllChannels.Position=[4 4 hFGUI.Position(3)-8 350];
 
+% Initialize the uipanel that contains the information for the plotter
 hpPlot=uipanel('parent',hFGUI,'backgroundcolor','w',...
     'units','pixels','Title','Plotter','fontsize',12);
 hpPlot.Position=[4 4 hFGUI.Position(3)-8 250];
 hpPlot.Position(2)=hpAllChannels.Position(2)+...
     hpAllChannels.Position(4)+2;
 
-%%%%%%%%%%%%%%% Channel Tables  %%%%%%%%%%%%
+%%%%%%%%%%%%%%% Analog Channel Table  %%%%%%%%%%%%
 
 % Analog channels table
-htbl_aCH=uitable('parent',hpAllChannels);
-htbl_aCH.RowName={};
+htbl_aCH=uitable('parent',hpAllChannels,'RowName',{},'FontName',...
+    'Monospaced');
 htbl_aCH.ColumnName={'#', 'Name','show'};
 htbl_aCH.ColumnWidth={20,150,40};
-htbl_aCH.FontName='monospaced';
 htbl_aCH.ColumnFormat={'char', 'char', 'logical'};
 htbl_aCH.ColumnEditable=[false false true];
 htbl_aCH.Position(3)=htbl_aCH.Extent(3)+17;
@@ -95,26 +109,34 @@ drawnow;
     htbl_aCH.Data{rr,3}=false;
  end 
  
- % callback function
+ % callback function for the analog table 
     function AtblCB(htbl,data)
+        % get what row is initiating the callback
         r=data.Indices(1);        
         
+        % check to see if the cell was edited
         if data.NewData && ~data.PreviousData
+            % if going from 0->1 add the channel to the selected list
             chNum=str2num(htbl.Data{r,1}); 
             ind=find([seqdata.analogchannels.channel]==chNum,1);
             aCH=seqdata.analogchannels(ind);
             aCHshow(end+1)=aCH;      
-            disp(['Showing analog CH' num2str(chNum,'%02.f') ' : ' aCH.name]);
+            disp(['Showing analog CH' ...
+                num2str(chNum,'%02.f') ' : ' aCH.name]);
             
         elseif ~data.NewData && data.PreviousData
+            % if going from 1->0 remove the channel from the selected list
             chNum=str2num(htbl.Data{r,1}); 
             ind=find([aCHshow.channel]==chNum,1);
-            disp(['Hiding  analog CH' num2str(chNum,'%02.f') ' : ' aCHshow(ind).name]);
+            disp(['Hiding  analog CH' ...
+                num2str(chNum,'%02.f') ' : ' aCHshow(ind).name]);
             aCHshow(ind)=[];   
         end 
+        % update the table of selected channels
         updateShowTBLA;
     end
 
+% string label for all available analog channels
 str='Analog Channels';
 tA=uicontrol('style','text','string',str,'fontweight','bold',...
     'fontname','arial','fontsize',10,'backgroundcolor','w',...
@@ -123,19 +145,19 @@ tA.Position(1)=htbl_aCH.Position(1);
 tA.Position(2)=htbl_aCH.Position(2)+htbl_aCH.Position(4)+5;
 tA.Position(3)=htbl_aCH.Position(3);
 
+%%%%%%%%%%%%%%% Digital Channel Table  %%%%%%%%%%%%
+
 % digital channels table
-htbl_dCH=uitable('parent',hpAllChannels);
-htbl_dCH.RowName={};
+htbl_dCH=uitable('parent',hpAllChannels,'RowName',{},'FontName',...
+    'Monospaced');
 htbl_dCH.ColumnName={'#', 'Name','show'};
 htbl_dCH.ColumnWidth={20,150,40};
-htbl_dCH.FontName='monospaced';
 htbl_dCH.ColumnFormat={'char', 'char', 'logical'};
 htbl_dCH.ColumnEditable=[false false true];
 htbl_dCH.Position(3)=htbl_dCH.Extent(3)+17;
 htbl_dCH.Position(2:4)=htbl_aCH.Position(2:4);
 htbl_dCH.Position(1)=htbl_aCH.Position(1)+htbl_aCH.Position(3)+5;
-
- htbl_dCH.CellEditCallback=@DtblCB;
+htbl_dCH.CellEditCallback=@DtblCB;
 
 % populate the table 
  for kk=1:length(seqdata.analogchannels)
@@ -148,24 +170,31 @@ htbl_dCH.Position(1)=htbl_aCH.Position(1)+htbl_aCH.Position(3)+5;
  
   % callback function
     function DtblCB(htbl,data)
-        r=data.Indices(1);        
+        % get what row is initiating the callback
+        r=data.Indices(1);       
         
+        % check to see if the cell was edited      
         if data.NewData && ~data.PreviousData
+            % if going from 0->1 add the channel to the selected list
             chNum=str2num(htbl.Data{r,1}); 
             ind=find([seqdata.digchannels.channel]==chNum,1);
             dCH=seqdata.digchannels(ind);
             dCHshow(end+1)=dCH;      
-            disp(['Showing digital CH' num2str(chNum,'%02.f') ' : ' dCH.name]);
+            disp(['Showing digital CH' num2str(chNum,'%02.f') ...
+                ' : ' dCH.name]);
             
         elseif ~data.NewData && data.PreviousData
+            % if going from 1->0 remove the channel from the selected list
             chNum=str2num(htbl.Data{r,1}); 
             ind=find([dCHshow.channel]==chNum,1);
-            disp(['Hiding  digital CH' num2str(chNum,'%02.f') ' : ' dCHshow(ind).name]);
+            disp(['Hiding  digital CH' num2str(chNum,'%02.f') ...
+                ' : ' dCHshow(ind).name]);
             dCHshow(ind)=[];   
         end 
         updateShowTBLD;
     end
 
+% string label for all available digital channels
 str='Digital Channels';
 tD=uicontrol('style','text','string',str,'fontweight','bold',...
     'fontname','arial','fontsize',10,'backgroundcolor','w',...
@@ -186,6 +215,7 @@ hbut_plot.Position(3:4)=[50 25];
 hbut_plot.Position(1)=5;
 hbut_plot.Position(2)=hpPlot.Position(4)-hbut_plot.Position(4)-40;
 hbut_plot.Callback=@plotCB;
+
     function plotCB(~,~)
        disp('i should plot something');        
        plottimes=htbl_time.Data;       
@@ -193,15 +223,12 @@ hbut_plot.Callback=@plotCB;
        tt=[dCHshow.channel]+length(seqdata.analogchannels);
        plotchannels=[plotchannels tt];
        
-%        [aTraces, dTraces]=generateTraces(seqdata);
-       
-       
-       PlotSequenceVersion2(sequencefunc,startcycle,plotchannels,plottimes);
-    
+%        [aTraces, dTraces]=generateTraces(seqdata);      
+       PlotSequenceVersion2(sequencefunc,startcycle,plotchannels,plottimes);    
     end
 
 
-% time table
+% time limits table
 htbl_time=uitable('parent',hpPlot);
 htbl_time.ColumnName={'start','end'};
 htbl_time.RowName={};
@@ -209,28 +236,25 @@ htbl_time.ColumnEditable=[true true];
 htbl_time.ColumnFormat={'numeric','numeric'};
 htbl_time.Data=[0 100000];
 htbl_time.ColumnWidth={80 80};
-
 htbl_time.Position(3:4)=htbl_time.Extent(3:4);
 htbl_time.Position(1)=hbut_plot.Position(1)+hbut_plot.Position(3)+5;
 htbl_time.Position(2)=hbut_plot.Position(2);
-
 htbl_time.CellEditCallback=@plotCB;
 
 
 % selected analog channels table
-htbl_SelaCH=uitable('parent',hpPlot);
-htbl_SelaCH.RowName={};
+htbl_SelaCH=uitable('parent',hpPlot,'RowName',{},'Fontname',...
+    'monospaced');
 htbl_SelaCH.ColumnName={'#', 'Name'};
 htbl_SelaCH.ColumnWidth={20,190};
-htbl_SelaCH.FontName='monospaced';
 htbl_SelaCH.ColumnFormat={'char', 'char'};
 htbl_SelaCH.ColumnEditable=[false false true];
-
 htbl_SelaCH.Position(4)=150;
 htbl_SelaCH.Position(3)=htbl_SelaCH.Extent(3)+17;
 htbl_SelaCH.Position(2)=4;
 htbl_SelaCH.Position(1)=htbl_aCH.Position(1);
 
+% callback for the selected analog channels table
     function updateShowTBLA
         htbl_SelaCH.Data={};
         [~,inds]=sort([aCHshow.channel]);
@@ -243,19 +267,18 @@ htbl_SelaCH.Position(1)=htbl_aCH.Position(1);
     end
 
 % selected analog channels table
-htbl_SeldCH=uitable('parent',hpPlot);
-htbl_SeldCH.RowName={};
+htbl_SeldCH=uitable('parent',hpPlot,'RowName',{},'Fontname',...
+    'monospaced');
 htbl_SeldCH.ColumnName={'#', 'Name'};
 htbl_SeldCH.ColumnWidth={20,190};
-htbl_SeldCH.FontName='monospaced';
 htbl_SeldCH.ColumnFormat={'char', 'char'};
 htbl_SeldCH.ColumnEditable=[false false true];
-
 htbl_SeldCH.Position(4)=150;
 htbl_SeldCH.Position(3)=htbl_SeldCH.Extent(3)+17;
 htbl_SeldCH.Position(2)=4;
 htbl_SeldCH.Position(1)=htbl_SelaCH.Position(1)+htbl_SelaCH.Position(3)+5;
 
+% callback for the selected digital channels table
     function updateShowTBLD
         htbl_SeldCH.Data={};
         [~,inds]=sort([dCHshow.channel]);

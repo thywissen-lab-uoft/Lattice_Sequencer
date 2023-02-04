03bf91266b298a233a8121f4d0a908cb02e1cba1
Cora <63414958+cjfujiwara@users.noreply.github.com>
2023-02-03 19:30:59 -0500

    edting load mag from mot asethetic

--- Sequence Files/Load Magtrap Sequence Files/magneto_optical_trap/Load_MagTrap_from_MOT.m
+++ Sequence Files/Load Magtrap Sequence Files/magneto_optical_trap/Load_MagTrap_from_MOT.m
@@ -4,41 +4,32 @@ global seqdata;
 
 curtime = timein;
 
-%% Magnetic Trapping
-% updated @ 2018-03-22
+
    
     ramp_time1 = 1;
     hold_time1 = 10;
-%     gradient_value1 = 0.5*(1.22 + 89.6*1);  
 
     gradient_value1_list=[0.75];
-    gradient_value1= getScanParameter(gradient_value1_list,seqdata.scancycle,seqdata.randcyclelist,'gradient_value1');  %in MHZ
+    gradient_value1= getScanParameter(gradient_value1_list,...
+        seqdata.scancycle,seqdata.randcyclelist,'gradient_value1');  %in MHZ
     gradient_value1=gradient_value1*(1.22 + 89.6*1);
-%     gradient_value1=gradient_value1*gradient_value2;
-
-%      ramp_time2_list = [50:2:100];
-%      ramp_time2 = getScanParameter(ramp_time2_list,seqdata.scancycle,seqdata.randcyclelist,'ramp_time2');  
 
     ramp_time2 = 50;
     hold_time2 = 10;
     gradient_value2 = 50;1.0*(1.22 + 89.6*1);
     
-    % Special ramp paramter CF
-%     gList=5:5:100;
-%     gradient_value1 = 100;
-%     gradient_value2 = getScanParameter(gList,seqdata.scancycle,seqdata.randcyclelist,'gradient');
-%     gradient_value2=30;
-    
     %Turn on MOT TTL
-    setDigitalChannel(calctime(curtime,0),16,0);
+    setDigitalChannel(calctime(curtime,0),'MOT TTL',0);
     %1st ramp
-    AnalogFuncTo(calctime(curtime,0),'MOT Coil',@(t,tt,y1,y2)(ramp_linear(t,tt,y1,y2)),ramp_time1,ramp_time1,gradient_value1);
+    AnalogFuncTo(calctime(curtime,0),'MOT Coil',...
+        @(t,tt,y1,y2)(ramp_linear(t,tt,y1,y2)),ramp_time1,ramp_time1,gradient_value1);
 
     
     curtime=calctime(curtime,ramp_time1+hold_time1);
 
     %2nd ramp
-    curtime = AnalogFuncTo(calctime(curtime,0),'MOT Coil',@(t,tt,y1,y2)(ramp_linear(t,tt,y1,y2)),ramp_time2,ramp_time2,gradient_value2);
+    curtime = AnalogFuncTo(calctime(curtime,0),'MOT Coil',...
+        @(t,tt,y1,y2)(ramp_linear(t,tt,y1,y2)),ramp_time2,ramp_time2,gradient_value2);
     % 2nd hold
     curtime = calctime(curtime, hold_time2);
 

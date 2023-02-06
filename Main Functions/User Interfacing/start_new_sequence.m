ceef466126bb63639ce8a1c4b3496c53357d8824
Cora <63414958+cjfujiwara@users.noreply.github.com>
2023-02-06 18:50:05 -0500

    adding variables and what not to output

--- Main Functions/User Interfacing/start_new_sequence.m
+++ Main Functions/User Interfacing/start_new_sequence.m
@@ -24,6 +24,15 @@ seqdata.compath='Y:\_communication';
 seqdata.camera_control_file = 'Y:\_communication\camera_control.mat';
 seqdata.analysis_summary_file = 'Y:\_communication\pco_analysis_summary.mat';
 
+if isfield(seqdata,'variables'); seqdata=rmfield(seqdata,'variables');end;
+if isfield(seqdata,'params'); seqdata=rmfield(seqdata,'params');end;
+if isfield(seqdata,'variables_units'); seqdata=rmfield(seqdata,'variables_units');end;
+if isfield(seqdata,'output_vars_vals'); seqdata=rmfield(seqdata,'output_vars_vals');end;
+if isfield(seqdata,'output_vars_units'); seqdata=rmfield(seqdata,'output_vars_units');end;
+if isfield(seqdata,'output_params'); seqdata=rmfield(seqdata,'output_params');end;
+
+
+
 % seqdata.multiscannum = [];%Feb-2017
 if isfield(seqdata,'times'); seqdata = rmfield(seqdata,'times'); end
 if isfield(seqdata,'flags'); seqdata = rmfield(seqdata,'flags'); end

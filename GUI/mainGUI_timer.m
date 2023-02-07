ac018fcf5b4ab3751534009624536d4b7aca1b87
Cora <63414958+cjfujiwara@users.noreply.github.com>
2023-02-07 00:23:51 -0500

    gui timer rename

--- GUI/mainGUI_timer.m
+++ GUI/mainGUI_timer.m
@@ -1,16 +1,12 @@
-function t = main_gui_timer(fig)
-
-
-  
+function t = mainGUI_timer(fig)
+global seqdata
 
 % The adwin progress timer object
-t=timer('Name','mytimer','ExecutionMode','FixedSpacing',...
+t=timer('Name','sequencer_gui_timer','ExecutionMode','FixedSpacing',...
     'TimerFcn',@update,'Period',.05,...
     'StopFcn',@stopAdwinTimer,'StartFcn',@startAdwinTimer);
 
 dQ = struct;
-dQ(1).Name = 'bob';
-dQ(2).Name = 'tom';
 
 t.UserData = struct;
 t.UserData.GUI = fig;
@@ -20,12 +16,19 @@ t.UserData.AdwinTime = 3;
 t.UserData.WaitTime = 2;
 t.UserData.Status = 'off';
 
+% Need to break up run sequence if want to use this because the timer
+% begings as soon as start fcn is called.
+
 % Function to run when the adwin starts the sequence.
 function startAdwinTimer(src,evt)        
+    if isempty(src.UserData.CommandQueue)
+        warning('No commands in queue to run.');
+       return; 
+    end
+    
     src.UserData.TimerStart = now;      % Start time
     src.UserData.ElapsedTime = 0;       % Reset timer
     q = src.UserData.CommandQueue(1);   % Get the next command to run
-    disp(q);
 end
 
 % Timer update function
@@ -35,18 +38,19 @@ function update(src,evt)
     dT_wait     = src.UserData.WaitTime;                    % Wait Time
     
     src.UserData.ElapsedTime = dT;      % Elapsed Time   
-    data=guidata(src.UserData);         % GUI Data
+    data=guidata(src.UserData.GUI);         % GUI Data
     
     if dT<=dT_adwin    
+        
         % Update partial adwin bar
         data.pAdWinBar.XData    = [0 1 1 0]*dT/dT_adwin;
         data.tAdWinTime1.String = [num2str(dT,'%.2f') ' s'];
-        data.tAdWinTime2.String = [num2str(dT0,'%.2f') ' s'];
+        data.tAdWinTime2.String = [num2str(dT_adwin,'%.2f') ' s'];
     else
         % Update complete adwin bar
         data.pAdWinBar.XData = [0 1 1 0];
-        data.tAdWinTime1.String = [num2str(dT0,'%.2f') ' s'];
-        data.tAdWinTime2.String = [num2str(dT0,'%.2f') ' s'];
+        data.tAdWinTime1.String = [num2str(dT_adwin,'%.2f') ' s'];
+        data.tAdWinTime2.String = [num2str(dT_adwin,'%.2f') ' s'];
         
         dTw = dT-dT_adwin;         
         if dTw<dT_wait
@@ -70,7 +74,10 @@ end
 
 % Function to evaluate when the sequence is done
 function stopAdwinTimer(src,evt)
-    src.UserData.CommandQueue(1)=[];
+    if ~isempty(src.UserData.CommandQueue)        
+        src.UserData.CommandQueue(1)=[];
+    end
+    
     if ~isempty(src.UserData.CommandQueue)
         start(src);
     else

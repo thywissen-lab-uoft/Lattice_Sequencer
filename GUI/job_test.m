ee5e8487a7ea412f6a00f1fcee57559ecfaa1dff
Cora <63414958+cjfujiwara@users.noreply.github.com>
2023-02-11 00:00:00 -0500

    Update job_test.m

--- GUI/job_test.m
+++ GUI/job_test.m
@@ -15,8 +15,7 @@ end
 funcs = {@main_settings,@seq_mod_1,@main_sequence};
 
 % add it to the queue
-j1 = sequencer_job(funcs,'K RF1B stats',1:2);
-
+j1 = sequencer_job(funcs,'K RF1B stats',1:20);
 
 %% XDT DFG TOF
 tofVec = [21 23 25];
@@ -28,7 +27,7 @@ tofVec = [21 23 25];
 
 funcs = {@main_settings,@seq_mod_2,@main_sequence};
 
-j2 = sequencer_job(funcs,'DFG TOF',1:3);
+j2 = sequencer_job(funcs,'DFG TOF',1:12);
 
 %% add jobs
 

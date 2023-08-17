cb2cb51f84d53ed418132ab2dd05bae42e0fd31e
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2023-08-17 16:00:20 -0400

    new splines for all

--- Sequence Files/Load Magtrap Sequence Files/transport/loadTransportCurve.m
+++ Sequence Files/Load Magtrap Sequence Files/transport/loadTransportCurve.m
@@ -43,7 +43,9 @@ switch coil_identifier
         y = dlmread(fullfile(mydir,filenames.PushCoil),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
         curr(1:1:366) = y;        
+        curr(pos==44)=[];pos(pos==44)=[];
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');   
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=45].*[xq>=0];           
     case 'MOT Coil'
         y = dlmread(fullfile(mydir,filenames.MOTCoil),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
@@ -53,88 +55,146 @@ switch coil_identifier
         y = dlmread(fullfile(mydir,filenames.Coil3),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
         curr(1:1:366) = y;        
-        pos2curr = @(xq) interp1(pos,curr,xq,'linear'); 
+        curr(103)=[];
+        pos(103)=[];               
+        pos2curr = @(xq) interp1(pos,curr,xq,'linear');          
+        pos2curr = @(xq) interp1(pos,curr,xq,'linear').*[xq>=100]+...
+            interp1(pos,curr,xq,'spline').*[xq<100];        
     case 'Coil 4'
         y = dlmread(fullfile(mydir,filenames.Coil4),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
-        curr(1:1:366) = y;        
-        pos2curr = @(xq) interp1(pos,curr,xq,'linear'); 
+        curr(1:1:366) = y;                
+        curr(133)=[];
+        pos(133)=[];               
+        pos2curr = @(xq) interp1(pos,curr,xq,'linear');          
+        pos2curr = @(xq) interp1(pos,curr,xq,'linear').*[xq>=130]+...
+            interp1(pos,curr,xq,'spline').*[xq<130].*[xq>=44];         
     case 'Coil 5'
         y = dlmread(fullfile(mydir,filenames.Coil5),',',0,1);
-        if length(y)~=366;error('on no, unexpected length');end
-        curr(1:1:366) = y;        
-        pos2curr = @(xq) interp1(pos,curr,xq,'linear');   
+        if length(y)~=366;error('on no, unexpected length');end   
+        curr(1:1:366) = y; 
+        curr(pos==68)=[];pos(pos==68)=[];
+        curr(pos==165)=[];pos(pos==165)=[];
+        pos2curr = @(xq) interp1(pos,curr,xq,'linear');        
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=166].*[xq>=67];   
     case 'Coil 6'
         y = dlmread(fullfile(mydir,filenames.Coil6),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
         curr(1:1:366) = y;        
+        curr(pos==194)=[];pos(pos==194)=[];
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');  
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=195].*[xq>=102];   
     case 'Coil 7'
         y = dlmread(fullfile(mydir,filenames.Coil7),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
         curr(1:1:366) = y;        
+        curr(pos==131)=[];pos(pos==131)=[];
+        curr(pos==227)=[];pos(pos==227)=[];
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');  
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=228].*[xq>=130];   
     case 'Coil 8'
         y = dlmread(fullfile(mydir,filenames.Coil8),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
-        curr(1:1:366) = y;        
-        pos2curr = @(xq) interp1(pos,curr,xq,'linear');         
+        curr(1:1:366) = y; 
+        curr(pos==166)=[];pos(pos==166)=[];
+        curr(pos==257)=[];pos(pos==257)=[];
+        pos2curr = @(xq) interp1(pos,curr,xq,'linear');    
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=258].*[xq>=165]; 
     case 'Coil 9'
         y = dlmread(fullfile(mydir,filenames.Coil9),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
-        curr(1:1:366) = y;        
+        curr(1:1:366) = y;  
+        curr(pos==195)=[];pos(pos==195)=[];
+        curr(pos==290)=[];pos(pos==290)=[];
         pos2curr = @(xq) interp1(pos,curr,xq,'linear'); 
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=291].*[xq>=194]; 
     case 'Coil 10'
         y = dlmread(fullfile(mydir,filenames.Coil10),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
-        curr(1:1:366) = y;        
+        curr(1:1:366) = y; 
+        curr(pos==228)=[];pos(pos==228)=[];
+        curr(pos==309)=[];pos(pos==309)=[];
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=310].*[xq>=227]; 
     case 'Coil 11'
         y = dlmread(fullfile(mydir,filenames.Coil11),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
-        curr(1:1:366) = y;        
+        curr(1:1:366) = y;  
+        curr(pos==258)=[];pos(pos==258)=[];
+        curr(pos==330)=[];pos(pos==330)=[];
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=331].*[xq>=257]; 
     case 'Coil Extra'
-        y = dlmread(fullfile(mydir,filenames.Coil1Extra),',',0,1);
+        y = dlmread(fullfile(mydir,filenames.CoilExtra),',',0,1);
         if length(y)~=366;error('on no, unexpected length');end
-        curr(1:1:366) = y;        
+        curr(1:1:366) = y;    
+        curr(pos==365)=[];pos(pos==365)=[];
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=366].*[xq>=297]; 
     case 'Coil 12a'
         yH = dlmread(fullfile(mydir,filenames.Coil12),',',0,1);
         yV = dlmread(fullfile(mydir,filenames.Coil12a),',',0,1);
         if length(yH)~=366;error('on no, unexpected length');end
-        if length(yV)~=174;error('on no, unexpected length');end        
+        if length(yV)~=174;error('on no, unexpected length');end    
         curr(1:1:366) = -yH;
         curr(367:end) = yV;
+        curr(pos==366)=[];pos(pos==366)=[];
+        curr(pos==367)=[];pos(pos==367)=[];
+        curr(pos==368)=[];pos(pos==368)=[];
+        curr(pos==432)=[];pos(pos==432)=[];
+        pos_h=pos(pos<=365);
+        curr_h = curr(pos<=365);        
+        pos_v = pos(pos>=365);
+        curr_v = curr(pos>=365);
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos_h,curr_h,xq,'spline').*[xq>=309].*[xq<=365] + ...
+            interp1(pos_v,curr_v,xq,'spline').*[xq>365].*[xq<=433];        
     case 'Coil 12b'
         yH = dlmread(fullfile(mydir,filenames.Coil12),',',0,1);
         yV = dlmread(fullfile(mydir,filenames.Coil12b),',',0,1);
         if length(yH)~=366;error('on no, unexpected length');end
         if length(yV)~=174;error('on no, unexpected length');end        
         curr(1:1:366) = yH;
-        curr(367:end) = yV;
+        curr(367:end) = yV;        
+        curr(pos==466)=[];pos(pos==466)=[];
+
+        pos_h=pos(pos<=365);
+        curr_h = curr(pos<=365);        
+        pos_v = pos(pos>=365);
+        curr_v = curr(pos>=365);       
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos_h,curr_h,xq,'spline',0).*[xq>=309].*[xq<=365] + ...
+        interp1(pos_v,curr_v,xq,'linear',0).*[xq>365].*[xq<=366] + ...
+        interp1(pos_v,curr_v,xq,'spline',0).*[xq>366].*[xq<=467]; 
     case 'Coil 13'
         y = dlmread(fullfile(mydir,filenames.Coil13),',',0,1);
         if length(y)~=174;error('on no, unexpected length');end
         curr(367:1:end) = y;        
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos1 = pos(pos<=366);
+        curr1 = curr(pos<=366);        
+        pos2 = pos(pos>=366);
+        curr2 = curr(pos>=366);        
+        pos2curr = @(xq) interp1(pos1,curr1,xq,'linear',0).*[xq>=365].*[xq<=366] + ...
+            interp1(pos2,curr2,xq,'spline',0).*[xq>366].*[xq<=518]; 
     case 'Coil 14'
         y = dlmread(fullfile(mydir,filenames.Coil14),',',0,1);
         if length(y)~=174;error('on no, unexpected length');end
         curr(367:1:end) = y;        
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=539].*[xq>=387]; 
     case 'Coil 15'
         y = dlmread(fullfile(mydir,filenames.Coil15),',',0,1);
         if length(y)~=174;error('on no, unexpected length');end
         curr(367:1:end) = y;        
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=539].*[xq>=432]; 
     case 'Coil 16'
         y = dlmread(fullfile(mydir,filenames.Coil16),',',0,1);
         if length(y)~=174;error('on no, unexpected length');end
         curr(367:1:end) = y;        
         pos2curr = @(xq) interp1(pos,curr,xq,'linear');
+        pos2curr = @(xq) interp1(pos,curr,xq,'spline').*[xq<=539].*[xq>=466]; 
 end
 
 

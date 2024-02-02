b49044150f714a0e1d42a636ed4d8bb9dc1be3a7
Cora <63414958+cjfujiwara@users.noreply.github.com>
2024-02-02 01:52:21 -0500

    updating conductivity job file

--- Jobs/job_conducivity_ac_shake.m
+++ Jobs/job_conducivity_ac_shake.m
@@ -1,36 +1,16 @@
 function J=job_conducivity_ac_shake
-    function curtime = bob(curtime,var)
-        global seqdata;
+    function curtime = ac_conductivity(curtime,freq,field,evap_depth)
+        global seqdata;        
+        % Define field, frequency, and evaporation depth
+        defVar('conductivity_FB_field',field,'G');       
+        defVar('conductivity_mod_freq',freq,'Hz');          
+        defVar('Evap_End_Power',evap_depth,'W');
         
-        field = var;
-        defVar('conductivity_FB_field',field,'G');
-       
-        freq = 54;
-        defVar('conductivity_mod_freq',freq,'Hz');  
-        
-        defVar('Evap_End_Power',0.08,'W');
-        
-        t0 = 50;
-        T = 1e3/freq;
-        tvec = round(t0 + linspace(0,3*T,30),1);
-        defVar('conductivity_mod_time',tvec,'ms');      % Modulation Time
-    end
-
-    function curtime = bob2(curtime,var)
-        global seqdata;
-        
-%         field = 170;
-%         defVar('conductivity_FB_field',field,'G');
-       
-        freq = var;
-        defVar('conductivity_mod_freq',freq,'Hz');  
-        
-%         defVar('Evap_End_Power',0.075,'W');
-        
+        % Modulation time vector
         t0 = 50;
         T = 1e3/freq;
         tvec = round(t0 + linspace(0,3*T,25),1);
-        defVar('conductivity_mod_time',tvec,'ms');      % Modulation Time
+        defVar('conductivity_mod_time',tvec,'ms');      
     end
 
 % This function evaluates at the end of each cycle
@@ -45,31 +25,24 @@ function J=job_conducivity_ac_shake
     function jobComplete
     end
 
-    function overridetoDFG(curtime)
-        global seqdata
-        seqdata.flags.lattice = 0;
-        defVar('tof',25,'ms');
-    end
 %%
-
 var_list = [20:10:160];
 var_list = var_list(randperm(numel(var_list)));
 clear J
 for ii = 1:length(var_list)
-    x = var_list(ii);
-    
+    f = var_list(ii);
+    B = 195;
+    pow = 0.085;    
     npt = struct;   
-    npt.SequenceFunctions   = {@main_settings,@(curtime) bob2(curtime,x),@main_sequence};
+    npt.SequenceFunctions   = {@main_settings,@(curtime) ac_conductivity(curtime,x),@main_sequence};
 %     npt.CycleStartFcn       = @cycleStart;
 %     npt.CycleCompleteFcn    = @cycleComplete;
 %     npt.JobCompleteFcn      = @jobComplete;
 
-%     if B>=200.5
-%         npt.ScanCyclesRequested = 1:41;
-%     else
-        npt.ScanCyclesRequested = 1:25;
-%     end
-    npt.JobName             = [num2str(ii) ' 2.5Er Modulate ' num2str(x)];
+
+    npt.ScanCyclesRequested = 1:25;
+    npt.JobName             = [num2str(ii) ' ' num2str(f) ' Hz,' ...
+        num2str(B) 'G,' num2str(1e3*pow) ' mW'];
     npt.SaveDirName         = npt.JobName;    
     J(ii) = sequencer_job(npt);
 end
